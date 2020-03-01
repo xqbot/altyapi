5a7e91c6cca14063de2bb63079e3d77d2ed84ca9
xq <60391249+xqbot@users.noreply.github.com>
2020-03-01 12:10:41 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,161 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const db = require("quick.db");
+const http = require("http");
+const express = require("express");
+require("./util/eventLoader")(client);
+//CodAre
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+//CodAre
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+//CodAre
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+//CodAre
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+client.on("guildMemberAdd", async(member) => {
+  let sunucupaneli = await db.fetch(`sunucupanel_${member.guild.id}`)
+  if(sunucupaneli) {
+    let rekoronline = await db.fetch(`panelrekor_${member.guild.id}`)
+    let toplamuye = member.guild.channels.find(x =>(x.name).startsWith("Toplam Üye •"))
+    let toplamaktif = member.guild.channels.find(x =>(x.name).startsWith("Aktif Üye •"))
+    let botlar = member.guild.channels.find(x =>(x.name).startsWith("Botlar •"))
+    let rekoraktif = member.guild.channels.find(x =>(x.name).startsWith("Rekor Aktiflik •"))
+    
+    if(member.guild.members.filter(off => off.presence.status !== 'offline').size > rekoronline) {
+      db.set(`panelrekor_${member.guild.id}`, member.guild.members.filter(off => off.presence.status !== 'offline').size)
+    }
+    try{
+      toplamuye.setName(`Toplam Üye • ${member.guild.members.size}`)
+      toplamaktif.setName(`Aktif Üye • ${member.guild.members.filter(off => off.presence.status !== 'offline').size}`)
+      botlar.setName(`Botlar • ${member.guild.members.filter(m => m.user.bot).size}`)
+      rekoraktif.setName(`Rekor Aktiflik • ${rekoronline}`)
+   } catch(e) { }
+  }
+})
+client.on("guildMemberRemove", async(member) => {
+  let sunucupaneli = await db.fetch(`sunucupanel_${member.guild.id}`)
+  if(sunucupaneli) {
+    let rekoronline = await db.fetch(`panelrekor_${member.guild.id}`)
+    let toplamuye = member.guild.channels.find(x =>(x.name).startsWith("Toplam Üye •"))
+    let toplamaktif = member.guild.channels.find(x =>(x.name).startsWith("Aktif Üye •"))
+    let botlar = member.guild.channels.find(x =>(x.name).startsWith("Botlar •"))
+    let rekoraktif = member.guild.channels.
+    find(x =>(x.name).startsWith("Rekor Aktiflik •"))
+    
+    if(member.guild.members.filter(off => off.presence.status !== 'offline').size > rekoronline) {
+      db.set(`panelrekor_${member.guild.id}`, member.guild.members.filter(off => off.presence.status !== 'offline').size)
+    }
+    try{
+      toplamuye.setName(`Toplam Üye • ${member.guild.members.size}`)
+      toplamaktif.setName(`Aktif Üye • ${member.guild.members.filter(off => off.presence.status !== 'offline').size}`)
+      botlar.setName(`Botlar • ${member.guild.members.filter(m => m.user.bot).size}`)
+      rekoraktif.setName(`Rekor Aktiflik • ${rekoronline}`)
+   } catch(e) { }
+  }
+})
+//CodAre
+client.login(ayarlar.token);
+
---
+++ ready.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../cT.json');
+
+var prefix = ayarlar.prefix;
+var version = ayarlar.version;
+var yapimci = ayarlar.yapimci;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+
+  client.user.setStatus("idle");
+   var oyun = [
+        "CodAre!"
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], {type: 'WATCHING'})      
+        }, 12000);
+  
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
